4807629d7daf3527ae8e2d3a876aa5f96afe8c81
khoa hoc du lieu 16a2 <ldthang.dhkl16a2hn@sv.uneti.edu.vn>
2022-11-17 16:16:29 +0700

    1st commit

---
+++ EXAMPLE/CHUONG 8/VD8.3.py
@@ -0,0 +1,19 @@
+diem_TB = eval(input("nhập điểm trung bình bằng ="))
+if diem_TB >=0 and diem_TB <=10 :
+      if diem_TB < 5 :
+            print("loại yêus kém")
+      elif diem_TB <6 :
+            print("xếp loại trung bình")
+      elif diem_TB<7 :
+            print("trung bình -khá")
+      elif diem_TB <8 :
+            print ("khá")
+      elif diem_TB <9 :
+            print("giỏi")
+      else :
+        print("xuâts xắc")
+else:
+      print("điểm nhập vào không hợp lệ")
+      
+
+
---
+++ EXAMPLE/CHUONG 8/vd8.4.py
@@ -0,0 +1,6 @@
+#đây là ví dụ về sử dụng vòng lặp
+#in các số từ 1 đến 10
+i=1 #i là biến đếm (biến điều khiến của vòng lặp)
+while i<=10 :
+    print("i=1", i)
+    i+=1
