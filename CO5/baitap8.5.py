3723170cca04e3b14e49a98b6a2e1152f39e3d43
khoa hoc du lieu 16a2 <ldthang.dhkl16a2hn@sv.uneti.edu.vn>
2022-11-24 16:14:05 +0700

    one

---
+++ CO5/baitap8.5.py
@@ -0,0 +1,10 @@
+a= int(input("nhap nam"))
+if  a%4=:0 and a%100!=0:
+     print("nam", a , "la nam nhuan")
+elif a%100=0
+   print("nam", a , "la nam nhuam" )
+else
+   print("nam", a , "khong la nam nhuan")
+
+
+ 
